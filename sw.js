ee65b079aaecc646e51a5d7a5d35b46d645ef137
Monica <monica_iqbal09@hotmail.com>
2025-11-10 14:44:07 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,8 +12,7 @@ self.addEventListener('install', function(e) {
             // If the request for any of these resources fails, _none_ of the resources will be
             // added to the cache.
             return cache.addAll([
-                '/',
-                '/index.html',
+                '/ud-app/',
                 '/manifest.json',
                 'https://unpkg.com/onsenui/css/onsenui.min.css',
                 'https://unpkg.com/onsenui/css/onsen-css-components.min.css',
