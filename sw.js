1ec974d71e086b1f906a31911ea71687540d1724
Monica <monica_iqbal09@hotmail.com>
2025-11-14 18:36:20 -0500

    removed extra slash from ud-app

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ self.addEventListener('install', function(e) {
             // If the request for any of these resources fails, _none_ of the resources will be
             // added to the cache.
             return cache.addAll([
-                '/ud-app/',
+                '/ud-app',
                 '/ud-app/src',
                 '/ud-app/manifest.json',
                 'https://unpkg.com/onsenui/css/onsenui.min.css',
@@ -35,7 +35,7 @@ self.addEventListener('fetch', function(e) {
 // make PWA available when it's offline
 caches.open('myonsenuipwa').then(cache => {
     return cache.addAll([
-        '/ud-app/',
+        '/ud-app',
         '/ud-app/src',
         '/ud-app/manifest.json',
         'https://unpkg.com/onsenui/css/onsenui.min.css',
